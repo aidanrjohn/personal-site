9b63a5711c65e068d471fd07f9859c8023418609
ajohn1215 <ajohn1215@gmail.com>
2016-12-21 05:21:11 +0000

    Started About Me section

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,6 +1,21 @@
 $(document).ready(function() { 
     $('#title').fadeTo(500, 1).css("display", "table");
+    
     $('.link').each(function(i) {
         $(this).delay((++i) * 400).fadeTo(500, 1);  
     });
+    
+    $("#about").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about-me").offset().top
+            }, 500);
+        $('#about-title').delay(500).fadeTo(500, 1).css("display", "table");
+        });
+        
+     $("#projects").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects-page").offset().top
+            }, 500);
+        
+        });
 }); 
