abf4c149fbddef045cf6846db02e64c9eae5d901
ajohn1215 <ajohn1215@gmail.com>
2016-12-21 19:29:38 +0000

    Finished About Me page

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -9,13 +9,27 @@ $(document).ready(function() {
         $('html, body').animate({
             scrollTop: $("#about-me").offset().top
             }, 500);
-        $('#about-title').delay(500).fadeTo(500, 1).css("display", "table");
-        });
+       aboutIn();
+    });
         
      $("#projects").click(function() {
         $('html, body').animate({
             scrollTop: $("#projects-page").offset().top
             }, 500);
         
-        });
-}); 
+     });
+     
+     $(document).on('scroll', function() {
+        if($(this).scrollTop()>=$('#about-me').position().top){
+            aboutIn();
+        }
+    });
+});
+
+function aboutIn() {
+    $('#about-title').delay(500).fadeTo(500, 1).css("display", "table");
+    $('#about-content').delay(1000).fadeTo(500,1);
+    $('.button-link').each(function(i) {
+        $(this).delay((i++) * 400 + 1500).fadeTo(500, 1);  
+    });
+}
