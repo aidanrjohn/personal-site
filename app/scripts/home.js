2442b8cf0375dee884b753b699542178e4563f0d
ajohn1215 <ajohn1215@gmail.com>
2016-12-29 18:18:19 +0000

    Fixed jQuery bug

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -2,17 +2,10 @@ $(document).ready(function() {
     
     initMap();
     
-    $('#title').fadeTo(500, 1).css("display", "table");
-    
-    $('.link').each(function(i) {
-        $(this).delay((++i) * 400).fadeTo(500, 1);  
-    });
-    
     $("#about").click(function() {
         $('html, body').animate({
             scrollTop: $("#about-me").offset().top
             }, 500);
-       aboutIn();
     });
         
      $("#projects").click(function() {
@@ -22,20 +15,7 @@ $(document).ready(function() {
         
      });
      
-     $(document).on('scroll', function() {
-        if($(this).scrollTop()>=$('#about-me').position().top){
-            aboutIn();
-        }
-    });
 });
-
-function aboutIn() {
-    $('#about-title').delay(500).fadeTo(500, 1).css("display", "table");
-    $('#about-content').delay(1000).fadeTo(500,1);
-    $('.button-link').each(function(i) {
-        $(this).delay((i++) * 400 + 1500).fadeTo(500, 1);  
-    });
-}
 
 function initMap() {
         var oakland = {lat: 40.438825, lng: -79.957155};
